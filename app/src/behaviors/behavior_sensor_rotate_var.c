0b9ced94d5f4a7f584638226fff4630f088a2f2d
E_huang <176415120+E4181@users.noreply.github.com>
2026-08-08 22:33:20 +0800

    feat: add parameter metadata to sensor rotate var behavior

--- app/src/behaviors/behavior_sensor_rotate_var.c
+++ app/src/behaviors/behavior_sensor_rotate_var.c
@@ -12,9 +12,36 @@
 
 #include "behavior_sensor_rotate_common.h"
 
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+
+static const struct behavior_parameter_value_metadata param_values[] = {
+    {
+        .display_name = "Key",
+        .type = BEHAVIOR_PARAMETER_VALUE_TYPE_HID_USAGE,
+    },
+};
+
+static const struct behavior_parameter_metadata_set param_metadata_set[] = {{
+    .param1_values = param_values,
+    .param1_values_len = ARRAY_SIZE(param_values),
+    .param2_values = param_values,
+    .param2_values_len = ARRAY_SIZE(param_values),
+}};
+
+static const struct behavior_parameter_metadata metadata = {
+    .sets_len = ARRAY_SIZE(param_metadata_set),
+    .sets = param_metadata_set,
+};
+
+#endif
+
 static const struct behavior_driver_api behavior_sensor_rotate_var_driver_api = {
     .sensor_binding_accept_data = zmk_behavior_sensor_rotate_common_accept_data,
-    .sensor_binding_process = zmk_behavior_sensor_rotate_common_process};
+    .sensor_binding_process = zmk_behavior_sensor_rotate_common_process,
+#if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+    .parameter_metadata = &metadata,
+#endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
+};
 
 #define SENSOR_ROTATE_VAR_INST(n)                                                                  \
     static struct behavior_sensor_rotate_config behavior_sensor_rotate_var_config_##n = {          \
